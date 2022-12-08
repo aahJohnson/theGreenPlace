914b1d77eaf1d0af0549aad4ef355dbf60b6636c
aJohnson <johnson9438@gmail.com>
2022-12-08 16:29:35 +0100

    changed the product links from homepage, and styling of event component

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,47 +1,33 @@
 import React, { Component } from "react";
 import "../styles/homePage.css";
+import instagramIcon from "../media/instagramIcon.png";
+import facebookIcon from "../media/facebookIcon.png";
+import youtubeIcon from "../media/youtubeIcon.png";
 
 const HomePage = () => {
   return (
     <main>
       <div class="products">
-        <div class="productLinks">
-          <div class="productDiv">
-            <h3>Nyhet</h3>
-            <img
-              class="productImage"
-              alt="ProductImage"
-              src="https://us.balmain.com/dw/image/v2/BHGX_PRD/on/demandware.static/-/Sites-balmain-mastercatalog/default/dw5628ee63/images/hi-res/YF1R6005KB074AMF.png?sw=375&sh=530"
-            ></img>
-            <div class="productInformation">
-              <p>Rosa kortklänning</p>
-              <button>Se mer</button>
-            </div>
-          </div>
-          <div class="productDiv">
-            <h3>Nyhet</h3>
-            <img
-              class="productImage"
-              alt="ProductImage"
-              src="https://cdn.shopify.com/s/files/1/0563/8989/4284/products/D155WFW22GREEN_600x_crop_center.png?v=1653399590"
-            ></img>
-            <div class="productInformation">
-              <p>Grön långklänning</p>
-              <button>Se mer</button>
-            </div>
-          </div>
-          <div class="productDiv">
-            <h3>Nyhet</h3>
-            <img
-              class="productImage"
-              alt="ProductImage"
-              src="https://cdn.shopify.com/s/files/1/0412/1912/9497/products/dress2_Custom_View_1_9c184a1c-5c7b-4ae4-bf95-92a511086418_500x.png?v=1650880649"
-            ></img>
-            <div class="productInformation">
-              <p>Svart klänning</p>
-              <button>Se mer</button>
-            </div>
-          </div>
+        <div class="productsHead">
+          <h2>Nyheter</h2>
+          <button>Se mer</button>
+        </div>
+        <div class="productGrid">
+          <img
+            class="productImage"
+            alt="ProductImage"
+            src="https://images.unsplash.com/photo-1597976618063-810eb50c84fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+          />
+          <img
+            class="productImage"
+            alt="ProductImage"
+            src="https://images.unsplash.com/photo-1537758948578-4494132f4c19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
+          />
+          <img
+            class="productImage"
+            alt="ProductImage"
+            src="https://images.unsplash.com/photo-1500259783852-0ca9ce8a64dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+          />
         </div>
       </div>
       <div class="about">
@@ -81,69 +67,85 @@ const HomePage = () => {
         </div>
       </div>
       <div class="events">
-        <h1 class="eventsTitle">Events</h1>
-        <div class="eventList">
+        <div class="eventGrid">
+          {/* Event tile */}
           <div class="eventTile">
-            <h3 class="eventName">The Green Place magasin</h3>
             <img
               class="eventImage"
-              alt="event one"
-              src="https://image.isu.pub/220822183847-b4266535bf54430611260eadfffe50c8/jpg/page_1_thumb_large.jpg"
-            ></img>
-            <p class="eventDescription">Låt dig inspireras av våra magasin</p>
+              alt="event"
+              src="https://images.unsplash.com/photo-1514679725149-ecaa6013d817?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            />
+            <div class="eventImageOverlay">
+              <h3 class="eventName">The Green Place magazine</h3>
+            </div>
           </div>
+          {/* Event tile */}
           <div class="eventTile">
-            <h3 class="eventName">The Green Place magasin</h3>
-            <p class="eventDescription">Låt dig inspireras av våra magasin</p>
+            <img
+              class="eventImage"
+              alt="event"
+              src="https://images.unsplash.com/photo-1574079771556-f09a65db11e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=677&q=80"
+            />
+            <div class="eventImageOverlay">
+              <h3 class="eventName">Up to 30% off</h3>
+            </div>
           </div>
+          {/* Event tile */}
           <div class="eventTile">
-            <h3 class="eventName">The Green Place magasin</h3>
-            <p class="eventDescription">Låt dig inspireras av våra magasin</p>
+            <img
+              class="eventImage"
+              alt="event"
+              src="https://images.unsplash.com/photo-1600096194534-95cf5ece04cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80"
+            />
+            <div class="eventImageOverlay">
+              <div class="socialMediaLink">
+                <img
+                  src={instagramIcon}
+                  alt="Instagram"
+                  class="socialMediaIcon"
+                />
+              </div>
+              <div class="socialMediaLink">
+                <img
+                  src={facebookIcon}
+                  alt="Facebook"
+                  class="socialMediaIcon"
+                />
+              </div>
+              <div class="socialMediaLink">
+                <img src={youtubeIcon} alt="Youtube" class="socialMediaIcon" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
       <div class="products">
-        <div class="productLinks">
-          <div class="productDiv">
-            <h3>Populär</h3>
+        <div class="productGrid">
+          <a href=" " class="productGridTiles">
+            <span class="productTitle">Populärt</span>
             <img
               class="productImage"
               alt="ProductImage"
-              src="https://images.veromoda.com/10265446/3935091/001/veromoda-normalpassformo-ringningkortklaanning-grey.png?v=a5da0402e8df2a0eb0ce7a4e3bf6a81a&width=1024&quality=90"
-            ></img>
-            <div class="productInformation">
-              <p>Grön kortklänning</p>
-              <button>Se mer</button>
-            </div>
-          </div>
-          <div class="productDiv">
-            <h3>Populär</h3>
+              src="https://images.unsplash.com/photo-1543311236-a7c3638299ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=626&q=80"
+            />
+          </a>
+          <a href=" " class="productGridTiles">
+            <span class="productTitle">Jeans</span>
             <img
               class="productImage"
               alt="ProductImage"
-              src="https://quickbutik.imgix.net/28209I/products/6242ce1921faa.png"
-            ></img>
-            <div class="productInformation">
-              <p>Svart kortklänning</p>
-              <button>Se mer</button>
-            </div>
-          </div>
-          <div class="productDiv">
-            <h3>Populär</h3>
+              src="https://images.unsplash.com/photo-1603217040830-34473db521a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80"
+            />
+          </a>
+          <a href=" " class="productGridTiles">
+            <span class="productTitle">Jackor</span>
             <img
               class="productImage"
               alt="ProductImage"
-              src="https://quickbutik.imgix.net/28209I/products/6242cfc558623.png"
-            ></img>
-            <div class="productInformation">
-              <p>Röd kortklänning</p>
-              <button>Se mer</button>
-            </div>
-          </div>
+              src="https://images.unsplash.com/photo-1614251056762-19fca947d703?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            />
+          </a>
         </div>
-      </div>
-      <div>
-        <h1 class="socialsTitle">Socials</h1>
       </div>
     </main>
   );
